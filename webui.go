7aa698235e67e7c78abe62f1519efeeb529bf890
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-28 11:34:40 +0100

    Page with list of clusters

--- webui.go
+++ webui.go
@@ -36,6 +36,39 @@ type ClusterConfiguration struct {
 	Reason        string `json:"reason"`
 }
 
+var controllerURL = ""
+
+func performReadRequest(url string) ([]byte, error) {
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("Communication error with the server %v", err)
+	}
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("Expected HTTP status 200 OK, got %d", response.StatusCode)
+	}
+	body, readErr := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
+
+	if readErr != nil {
+		return nil, fmt.Errorf("Unable to read response body")
+	}
+
+	return body, nil
+}
+
+func readListOfClusters(controllerUrl string, apiPrefix string) ([]Cluster, error) {
+	clusters := []Cluster{}
+
+	url := controllerUrl + apiPrefix + "client/cluster"
+	body, err := performReadRequest(url)
+
+	err = json.Unmarshal(body, &clusters)
+	if err != nil {
+		return nil, err
+	}
+	return clusters, nil
+}
+
 func getContentType(filename string) string {
 	// TODO: to map
 	if strings.HasSuffix(filename, ".html") {
@@ -67,11 +100,37 @@ func staticPage(filename string) func(writer http.ResponseWriter, request *http.
 	}
 }
 
+type ListClustersDynContent struct {
+	Items []Cluster
+}
+
+func listClusters(writer http.ResponseWriter, request *http.Request) {
+	clusters, err := readListOfClusters(controllerURL, API_PREFIX)
+	if err != nil {
+		log.Println("Error reading list of clusters", err)
+		return
+	}
+
+	t, err := template.ParseFiles("html/list_clusters.html")
+	if err != nil {
+		writer.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(writer, "Not found!")
+		return
+	}
+
+	dynData := ListClustersDynContent{Items: clusters}
+	err = t.Execute(writer, dynData)
+	if err != nil {
+		println("Error executing template")
+	}
+}
+
 func startHttpServer() {
 	http.HandleFunc("/", staticPage("html/index.html"))
 	http.HandleFunc("/bootstrap.min.css", staticPage("html/bootstrap.min.css"))
 	http.HandleFunc("/bootstrap.min.js", staticPage("html/bootstrap.min.js"))
 	http.HandleFunc("/ccx.ccs", staticPage("html/ccs.ccs"))
+	http.HandleFunc("/list-clusters", listClusters)
 	http.ListenAndServe(":8888", nil)
 }
 
@@ -84,6 +143,8 @@ func main() {
 	if err != nil {
 		panic(fmt.Errorf("Fatal error config file: %s \n", err))
 	}
+
+	controllerURL = viper.GetString("URL")
 
 	log.Println("Starting the service")
 	startHttpServer()
