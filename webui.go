70e3af26c168ee5d982041868a51fb9c24b8a660
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-28 10:19:33 +0100

    Static web page serving

--- webui.go
+++ webui.go
@@ -3,8 +3,31 @@ package main
 import (
 	"fmt"
 	"github.com/spf13/viper"
+	"io/ioutil"
 	"log"
+	"net/http"
 )
+
+func sendStaticPage(writer http.ResponseWriter, filename string) {
+	body, err := ioutil.ReadFile(filename)
+	if err == nil {
+		fmt.Fprint(writer, string(body))
+	} else {
+		writer.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(writer, "Not found!")
+	}
+}
+
+func staticPage(filename string) func(writer http.ResponseWriter, request *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		sendStaticPage(writer, filename)
+	}
+}
+
+func startHttpServer() {
+	http.HandleFunc("/", staticPage("html/index.html"))
+	http.ListenAndServe(":8888", nil)
+}
 
 func main() {
 	log.Println("Reading configuration")
@@ -17,4 +40,5 @@ func main() {
 	}
 
 	log.Println("Starting the service")
+	startHttpServer()
 }
