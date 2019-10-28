ea2f75ae770dee1e685f9be37e4efccdb82e37a2
Pavel Tisnovsky <ptisnovs@redhat.com>
2019-10-28 10:51:17 +0100

    Front page

--- webui.go
+++ webui.go
@@ -6,11 +6,26 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"strings"
 )
+
+func getContentType(filename string) string {
+	// TODO: to map
+	if strings.HasSuffix(filename, ".html") {
+		return "text/html"
+	} else if strings.HasSuffix(filename, ".js") {
+		return "application/javascript"
+	} else if strings.HasSuffix(filename, ".css") {
+		return "text/css"
+	}
+	return "text/html"
+}
 
 func sendStaticPage(writer http.ResponseWriter, filename string) {
 	body, err := ioutil.ReadFile(filename)
 	if err == nil {
+		writer.Header().Set("Server", "A Go Web Server")
+		writer.Header().Set("Content-Type", getContentType(filename))
 		fmt.Fprint(writer, string(body))
 	} else {
 		writer.WriteHeader(http.StatusNotFound)
@@ -19,6 +34,7 @@ func sendStaticPage(writer http.ResponseWriter, filename string) {
 }
 
 func staticPage(filename string) func(writer http.ResponseWriter, request *http.Request) {
+	log.Println("Serving static file", filename)
 	return func(writer http.ResponseWriter, request *http.Request) {
 		sendStaticPage(writer, filename)
 	}
@@ -26,6 +42,9 @@ func staticPage(filename string) func(writer http.ResponseWriter, request *http.
 
 func startHttpServer() {
 	http.HandleFunc("/", staticPage("html/index.html"))
+	http.HandleFunc("/bootstrap.min.css", staticPage("html/bootstrap.min.css"))
+	http.HandleFunc("/bootstrap.min.js", staticPage("html/bootstrap.min.js"))
+	http.HandleFunc("/ccx.ccs", staticPage("html/ccs.ccs"))
 	http.ListenAndServe(":8888", nil)
 }
 
