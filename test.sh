3aeac2645f6dde8cf23f84f758742ea9bf5a3c4b
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-03-06 08:06:30 +0100

    Updated CI setup

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+go test -race -covermode=atomic -coverprofile coverage.out "$(go list ./... | grep -v tests)" "$@"
+exit_code=$?
+
+echo "Test coverage:"
+go tool cover -func=coverage.out
+
+exit $exit_code
